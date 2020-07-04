20021e524712b98ec231a6ec4314e24ed5fcd471
shubham <shubhamm829@gmail.com>
2020-07-04 15:54:33 +0530

    Initial  comit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -40,7 +40,7 @@ class _HomeState extends State<Home> {
     if(switching_val){
       this.getuser_Location();
     }else{
-      zipCodeControlled.text = "" ;
+      zipCodeControlled.text = "23" ;
     }
 
   }
